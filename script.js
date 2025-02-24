8cbef502d6db0ed20117095670598f1742b79509
Lokesh Nalamasa <107922763+lokeshgoud15@users.noreply.github.com>
2025-02-24 10:36:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function dayOfYear(year) {
+	if(year%4==0 && year%100!==0 || year%400===0){
+		return 366
+	}
+	else{
+		return 365
+	}
+}
+console.log(dayOfYear(2022))
+console.log(dayOfYear(2024))
+console.log(dayOfYear(1900))
+console.log(dayOfYear(2000))
+
+dayOfYear(2000)
